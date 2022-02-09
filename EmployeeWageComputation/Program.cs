0d76fe7ab91042b41aae21afca1cb4fe8f080fd5
Devendra533 <97461548+Devendra533@users.noreply.github.com>
2022-02-09 12:43:19 +0530

    Added Employee Welcome Message

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWageComputation;
 
+
+Console.WriteLine("Employee WAge Computation");
 EmployeeOperation.GetAttendance();
 Console.ReadKey();
